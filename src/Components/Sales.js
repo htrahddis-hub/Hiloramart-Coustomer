dfb4bdcc9b7ecd598d42b71b9839b74b051b6804
Siddharth Bhatnagar <siddharthb1604@gmail.com>
2022-09-08 11:43:13 +0530

    chnaged date range picker

--- src/Components/Sales.js
+++ src/Components/Sales.js
@@ -3,48 +3,45 @@ import { AuthContext } from "../Context/AuthContext";
 import "../Styles/Components/Sales.css";
 import MySaleProduct from "./MySaleProduct";
 import { VENDOR_ALL_SALE } from "../Context/Types";
+import Calendar from "react-calendar";
 import { useEffect } from "react";
+import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
 
-const getCurrentDate = (separator = "") => {
-  let newDate = new Date();
-  let date = newDate.getDate();
-  let month = newDate.getMonth() + 1;
-  let year = newDate.getFullYear();
+const getFirstDayofMonth = () => {
+  var dt = new Date();
+  var firstDay = new Date(dt.getFullYear(), dt.getMonth(), 1);
 
-  return `${year}${separator}${
-    month < 10 ? `0${month}` : `${month}`
-  }${separator}${date < 10 ? `0${date}` : `${date}`}`;
+  return firstDay;
 };
 
 const getISODate = (date) => {
-  const darr = date.split("-");
-  const dobj = new Date(
-    parseInt(darr[0]),
-    parseInt(darr[1]) - 1,
-    parseInt(darr[2]) + 1
-  );
-  return dobj.toISOString();
+  return date.toISOString();
+};
+
+export const getFormatedDate = (date, separator = "") => {
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+
+  return `${day < 10 ? `0${day}` : `${day}`}${separator}${
+    month < 10 ? `0${month}` : `${month}`
+  }${separator}${year}`;
 };
 
 function Sales() {
   const { dispatch } = useContext(AuthContext);
-  const [date, setDate] = useState({
-    firstDate: getCurrentDate("-"),
-    lastDate: getCurrentDate("-"),
-  });
+  const [dateRange, setDateRange] = useState([
+    getFirstDayofMonth(),
+    new Date(),
+  ]);
   const [sales, setSales] = useState([]);
-
-  const handleChange = (e) => {
-    setDate((old) => {
-      return { ...old, [e.target.name]: e.target.value };
-    });
-  };
+  const [dropdown, setDropdown] = useState(false);
 
   const getSales = async () => {
     dispatch({
       type: VENDOR_ALL_SALE,
-      startDate: getISODate(date.firstDate).substring(0, 10),
-      endDate: getISODate(date.lastDate).substring(0, 10),
+      startDate: getISODate(dateRange[0]).substring(0, 10),
+      endDate: getISODate(dateRange[1]).substring(0, 10),
       page: 1,
       limit: 8,
       upDateState: setSales,
@@ -53,7 +50,16 @@ function Sales() {
 
   useEffect(() => {
     getSales();
-  }, [date]);
+  }, [dateRange]);
+
+  const handleDate = (e) => {
+    setDateRange(e);
+    setDropdown(false);
+  };
+
+  const handleDropdown = () => {
+    setDropdown((old) => !old);
+  };
 
   return (
     <div className="sales-cont">
@@ -61,29 +67,22 @@ function Sales() {
         <div style={{ textAlign: "center" }}>MY SALE</div>
       </div>
       <div className="d-flex justify-content-end me-5 mb-3">
-        <div className="d-flex align-items-center">
-          <label className="h5 me-2" htmlFor="start">
-            Start Date
-          </label>
-          <input
-            type="date"
-            id="start"
-            name="firstDate"
-            value={date.firstDate}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="d-flex align-items-center">
-          <label className="ms-3 me-2 h5" htmlFor="end">
-            End Date
-          </label>
-          <input
-            type="date"
-            id="end"
-            name="lastDate"
-            value={date.lastDate}
-            onChange={handleChange}
-          />
+        <div>
+          <div className="h5 bold" onClick={handleDropdown}>
+            <KeyboardArrowDownOutlinedIcon fontSize="large" />
+            {getFormatedDate(dateRange[0], "/")}
+            {" -- "}
+            {getFormatedDate(dateRange[1], "/")}
+          </div>
+          {dropdown && (
+            <Calendar
+              className="calendar"
+              selectRange
+              showNavigation
+              onChange={handleDate}
+              value={dateRange}
+            />
+          )}
         </div>
       </div>
       <div className="sale-product-parent">
