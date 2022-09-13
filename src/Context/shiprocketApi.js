7eede52fe16a5d599ce921ea5c48fafdfad23e69
Amit Sharma <iamamitshrma@gmail.com>
2022-09-13 11:23:07 +0530

    fix

--- src/Context/shiprocketApi.js
+++ src/Context/shiprocketApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'https://apiv2.shiprocket.in/v1'
+    baseURL: 'https://cors-fix-kylo.herokuapp.com/https://apiv2.shiprocket.in/v1'
 });
 
 
